832b0250f41b29535c63da55d6b8ef966be15150
jeff-sexton <sexton.jeffp@gmail.com>
2020-06-12 14:48:44 -0700

    Add table to cart component

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,7 +1,89 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles({
+  root: {
+    width: 700,
+    margin: "auto",
+  },
+  table: {
+    minWidth: 650,
+  },
+});
 
 const Cart = () => {
-  return <section>Cart</section>;
+  const classes = useStyles();
+
+  const [cartContents, setCartContents] = useState({ items: [], total: 0 });
+
+  useEffect(() => {
+    const cart = localStorage.getItem("cart")
+      ? JSON.parse(localStorage.getItem("cart"))
+      : {};
+
+    const cartArray = [];
+    let total = 0;
+
+    for (const itemId in cart) {
+      total += cart[itemId].total;
+      cartArray.push(cart[itemId]);
+    }
+
+    setCartContents({ items: cartArray, total });
+
+    console.log(cart);
+  }, []);
+
+  const cartRows = cartContents.items.map((item) => (
+    <TableRow key={item.product.id}>
+      <TableCell component="th" scope="row">
+        {item.product.title}
+      </TableCell>
+      <TableCell align="right">${item.itemPrice / 100.0}</TableCell>
+      <TableCell align="right">{item.quantity}</TableCell>
+      <TableCell align="right">${item.total / 100.0}</TableCell>
+    </TableRow>
+  ));
+
+  return (
+    <section>
+      Cart
+      <TableContainer className={classes.root} component={Paper}>
+        <Table className={classes.table} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Product</TableCell>
+              <TableCell align="right">Price</TableCell>
+              <TableCell align="right">Quantity</TableCell>
+              <TableCell align="right">Total</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {cartRows}
+            <TableRow>
+              <TableCell>Total</TableCell>
+              <TableCell />
+              <TableCell />
+              <TableCell>${cartContents.total / 100.0}</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <Button variant="contained" color="primary">
+        Checkout
+      </Button>
+    </section>
+  );
 };
 
 export default Cart;
